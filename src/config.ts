3f791d03bd7b19530c6f21a551a001f28b932de0
Matt Olive <58194219+mattaolive@users.noreply.github.com>
2024-02-26 16:08:50 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 
 
-export const SITE_NAME = 'Nice Tat'
+export const SITE_NAME = ''
 
 
-export const DESCRIPTION = 'Astro Site Template inspired by Casey Neistat’s ultra minimal personal site. '
+export const DESCRIPTION = ''
